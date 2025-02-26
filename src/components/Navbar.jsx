e90f6060b1f80913108b52ab3d500d85dd83d43d
SiharaH <siharaedirisinghe03@gmail.com>
2025-02-26 22:15:24 +0530

    Edited Navbar component

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,8 +1,11 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { assets } from '../assets/assets'
-import { NavLink } from 'react-router-dom'
+import { Link, NavLink } from 'react-router-dom'
 
 const Navbar = () => {
+
+    const [visible,setVisible] = useState(false)
+
   return (
     <div className='flex items-center justify-between py-5 font-medium'>
         <img src={assets.logo} alt="Logo" className='w-36' />
@@ -27,9 +30,38 @@ const Navbar = () => {
                 <p>CONTACT</p>
                 <hr className='w-2/4 border-none h-[1.5px] bg-pink-700 hidden' />
             </NavLink>
-
-
         </ul>
+
+        <div className='flex items-center gap-6'>
+            <img src={assets.search} className='w-5 cursor-pointer' alt="search" />
+
+            <div className='group relative'>
+                <img src={assets.login} alt="User-Account" className='w-5 cursor-pointer' />
+                <div className='group-hover:block hidden absolute dropdown-menu right-0 pt-4'>
+                    <div className='flex flex-col gap-2 w-36 py-3 px-5 bg-slate-100 text-gray-500 rounded'>
+                        <p className='cursor-pointer hover:text-black'>My Profile</p>
+                        <p className='cursor-pointer hover:text-black'>Orders</p>
+                        <p className='cursor-pointer hover:text-black'>Log Out</p>
+                    </div>
+                </div>
+            </div>
+            <Link to="/cart" className='relative'>
+                <img src={assets.cart} alt="" className='w-5 m-w-5' />
+                <p className='absolute right-[-5px] bottom-[-5px] w-3 text-center leading-3 bg-black text-white aspect-square rounded-full text-[6px]'>10</p>
+            </Link>
+
+            <img onClick={()=>setVisible(true)} src={assets.menu} className='w-5 cursor-pointer sm:hidden' />
+        </div>
+        {/* <div className='absolute top-0 right-0 bottom-0 overflow-hidden bg-white transition-all ${visible ? "w-full" : "w-0"}'>
+            <div className='flex flex-col text-gray-600'>
+                <div className='flex items-center gap-4 p-3'>
+                    <img className='h-4' src={assets.back} />
+                </div>
+
+            </div>
+
+        </div> */}
+
       
     </div>
   )
