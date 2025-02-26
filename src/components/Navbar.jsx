c130dd2e7fc1afaa56c997bfddf35475beb4ccd4
SiharaH <siharaedirisinghe03@gmail.com>
2025-02-26 13:11:00 +0530

    Create pages and added routes for each pages linking the navigation bar

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,38 @@
+import React from 'react'
+import { assets } from '../assets/assets'
+import { NavLink } from 'react-router-dom'
+
+const Navbar = () => {
+  return (
+    <div className='flex items-center justify-between py-5 font-medium'>
+        <img src={assets.logo} alt="Logo" className='w-36' />
+
+        <ul className='hidden sm:flex gap-5 text-sm text-pink-700'>
+            <NavLink to='/' className='flex flex-col items-center gap-1'>
+                <p>HOME</p>
+                <hr className='w-2/4 border-none h-[1.5px] bg-pink-700 hidden' />
+            </NavLink>
+
+            <NavLink to='/collection' className='flex flex-col items-center gap-1'>
+                <p>COLLECTION</p>
+                <hr className='w-2/4 border-none h-[1.5px] bg-pink-700 hidden' />
+            </NavLink>
+
+            <NavLink to='/about' className='flex flex-col items-center gap-1'>
+                <p>ABOUT</p>
+                <hr className='w-2/4 border-none h-[1.5px] bg-pink-700 hidden' />
+            </NavLink>
+
+            <NavLink to='/contact' className='flex flex-col items-center gap-1'>
+                <p>CONTACT</p>
+                <hr className='w-2/4 border-none h-[1.5px] bg-pink-700 hidden' />
+            </NavLink>
+
+
+        </ul>
+      
+    </div>
+  )
+}
+
+export default Navbar
