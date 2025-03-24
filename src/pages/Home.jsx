73bcfef592cd9a56bc98d155790a21e090b0767e
SiharaH <siharaedirisinghe03@gmail.com>
2025-03-25 00:24:35 +0530

    Edited Best Seller componant

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,12 +2,14 @@ import React from 'react'
 import Hero from '../components/Hero'
 import Policy from '../components/Policy'
 import LatestCollection from '../components/LatestCollection'
+import BestSeller from '../components/BestSeller'
 
 const Home = () => {
   return (
     <div>
       <Hero />
       <LatestCollection/>
+      <BestSeller/>
       <Policy />
     </div>
   )
