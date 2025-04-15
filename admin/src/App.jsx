1a06c8da41e8944d337cca14b26840d7c233c6a3
SiharaH <siharaedirisinghe03@gmail.com>
2025-04-15 23:49:47 +0530

    Admin Dashboard

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -1,11 +1,20 @@
 import React from 'react'
 import NavBar from './components/NavBar'
+import SideBar from './components/SideBar'
 
 const App = () => {
   return (
-    <>
+    <div className='bg-gray-50 min-h-screen'>
+      <>
       <NavBar />
-    </>
+      <hr />
+      <div className='flex w-full'>
+      <SideBar/>
+      </div>
+      
+      </>
+      
+    </div>
   )
 }
 
